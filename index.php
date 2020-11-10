ddf708aac3112ebdc8b4fe8800f31557e78d4729
m02xime <maxkb02@gmail.com>
2020-11-10 10:27:08 +0100

    update

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-<<<<<<< HEAD
-=======
 include('inc/pdo.php');
 include('inc/function.php');
 echo 'test';
@@ -16,4 +14,3 @@ include('inc/header-front.php');?>
 
 
 <?php include('inc/footer-front.php'); ?>
->>>>>>> 0f4b1e1d39c1e1c843cce3f1c22b208ead12d680
