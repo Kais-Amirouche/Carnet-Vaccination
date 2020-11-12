0c8bb950a76da4594d4ca7225b2bbd819149d44a
hhhallan <allanstm76@gmail.com>
2020-11-12 22:48:16 +0100

    questions admin

--- index.php
+++ index.php
@@ -15,7 +15,10 @@ include('inc/header.php');
 <h2>steven</h2>
 
   <div class="contact">
+    <?php if(isLogged()) { ?>
     <p>Besoin d'aide ? Posez-vos <a href="contact.php">questions</a></p>
+    <p><a href="mesquestions.php">Mes questions</a></p>
+    <?php } ?>
   </div>
 
 
