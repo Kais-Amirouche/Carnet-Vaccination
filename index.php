49d068b7d4e9b4032d95165682ed834607f3b3d9
Amirouche Kais <amirouche@amirouche.home>
2020-11-13 12:41:37 +0100

    info

--- index.php
+++ index.php
@@ -10,10 +10,9 @@ include('inc/function.php');
 include('inc/header.php');?>
 
 <!-- <h1>Accueil</h1> -->
-<div class="accimage">
+
   <img src="asset/img/vac-vac.jpeg" width="100%" alt="">
-  <h1><strong>Bienvenue</strong></h1>
-</div>
+
 
 
 
