dafe46f1955737b9846b9f0d044d66dd6660d991
safichou3 <>
2020-11-13 12:17:04 +0100

    update

--- allvaccin.php
+++ allvaccin.php
@@ -1,24 +1,16 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="asset/css/style2.css">
-    <title></title>
-  </head>
-  <body>
-    <header>
-      <a href="singlevaccin.php">La Diphtérie</a>
-      <a href="singlevaccin.php">Le Tétanos</a>
-      <a href="singlevaccin.php">La Poliomyélite</a>
-      <a href="singlevaccin.php">l’Haemophilus influenzae B</a>
-      <a href="singlevaccin.php">La Coqueluche</a>
-      <a href="singlevaccin.php">Hépatite B </a>
-      <a href="singlevaccin.php">La rougeole </a>
-      <a href="singlevaccin.php">Les oreillons</a>
-      <a href="singlevaccin.php">La rubéole</a>
-      <a href="singlevaccin.php">Le méningocoque C</a>
-      <a href="singlevaccin.php">Le pneumocoque</a>
-    </header>
-  </body>
-</html>
+<?php
+
+include('inc/pdo.php');
+include('inc/function.php');?>
+
+<a href="singlevaccin.php">La Diphtérie</a>
+<a href="singlevaccin.php">Le Tétanos</a>
+<a href="singlevaccin.php">La Poliomyélite</a>
+<a href="singlevaccin.php">l’Haemophilus influenzae B</a>
+<a href="singlevaccin.php">La Coqueluche</a>
+<a href="singlevaccin.php">Hépatite B </a>
+<a href="singlevaccin.php">La rougeole </a>
+<a href="singlevaccin.php">Les oreillons</a>
+<a href="singlevaccin.php">La rubéole</a>
+<a href="singlevaccin.php">Le méningocoque C</a>
+<a href="singlevaccin.php">Le pneumocoque</a>
