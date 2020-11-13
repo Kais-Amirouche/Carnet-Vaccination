26ba55785164f55c0a7d2283a501eb2abb4c5b50
m02xime <maxkb02@gmail.com>
2020-11-13 13:53:55 +0100

    profil update nice

--- monprofil.php
+++ monprofil.php
@@ -13,11 +13,17 @@ if(!empty($_SESSION['user']['id'])) {
     $query->execute(array($_SESSION['user']['id']));
     $user = $query->fetch();
     debug($user);
-
+    if(isset($_POST['prenom']) AND !empty($_POST['prenom']) AND $_POST['prenom'] != $user['prenom']) {
+       $newprenom = cleanXss($_POST['prenom']);
+       $insertmail = $pdo->prepare("UPDATE vac_users SET email = ? WHERE id = ?");
+       $insertmail->execute(array($newprenom, $user['id']));
+       $valid= '<p style="color:green;">ce champ a bien été modifié</p>';
+    }
    if(isset($_POST['newmail']) AND !empty($_POST['newmail']) AND $_POST['newmail'] != $user['email']) {
       $newmail = cleanXss($_POST['newmail']);
-      $insertmail = $pdo->prepare("UPDATE vac_users SET mail = ? WHERE id = ?");
+      $insertmail = $pdo->prepare("UPDATE vac_users SET email = ? WHERE id = ?");
       $insertmail->execute(array($newmail, $user['id']));
+      $valid= '<p style="color:green;">ce champ a bien été modifié</p>';
    }
    if(isset($_FILES['avatar']) AND !empty($_FILES['avatar']['name'])) {
     $tailleMax = 2097152;
@@ -53,10 +59,10 @@ if(!empty($_SESSION['user']['id'])) {
 include('inc/header.php');?>
 <h2>Edition de mon profil</h2>
 <form method="POST" action="" enctype="multipart/form-data">
-  <label>prenom :</label>
+  <label>prenom :<?php if(!empty($valid)){ echo $valid; } ?></label>
   <input type="text" name="prenom" id="prenom" class="form-control" value="<?php if(!empty($_POST['prenom'])) { echo $_POST['prenom']; }else {echo $user['prenom'];} ?>" placeholder="prenom" />
   <span class="error"><?php if(!empty($errors['prenom'])) { echo $errors['prenom']; } ?></span>
-  <label>Email :</label>
+  <label>Email :<?php if(!empty($valid)){ echo $valid; } ?></label>
   <input type="text" name="newmail" placeholder="Email" value="<?php echo $user['email']; ?>" /><br /><br />
   <!-- PASSWORD1 -->
   <label>Mot de Passe :</label>
@@ -67,7 +73,7 @@ include('inc/header.php');?>
   <input type="password" name="password2" id="password2" class="form-control" value="" placeholder="Confirmer Le Mot De Passe"/>
   <span class="error"><?php if(!empty($errors['password2'])) { echo $errors['password2']; } ?></span>
   <!-- photo de profil -->
-  <label>photo de profil:</label>
+  <label>photo de profil:<?php if(!empty($errors['avatar'])) { echo $errors['avatar']; } ?></label>
   <input type="file" name="avatar" value="">
   <span class="error"><?php if(!empty($errors['avatar'])) { echo $errors['avatar']; } ?></span>
   <input type="submit" value="Mettre à jour mon profil !" />
