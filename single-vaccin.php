545eedd68e32120d985de5f6233c68b1a196b04a
safichou3 <>
2020-11-13 11:43:48 +0100

    update

---
+++ single-vaccin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/style2.css">
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <h1>Les vaccins</h1>
+      <h2>Obligatoires:</h2>
+          <nav>
+              <ul>
+                  <li><a href="Diphtérie.php">Diphtérie</a></li>
+                  <li><a href="tetanos.php">Tétanos</a></li>
+                  <li><a href="Poliomyélite.php">Poliomyélite</a></li>
+                  <li><a href="influenzae.php">l’Haemophilus influenzae B </a></li>
+                  <li><a href="Coqueluche.php">Coqueluche</a></li>
+                  <li><a href="Hépatite.php">Hépatite B </a></li>
+                  <li><a href="rougeole.php">rougeole</a></li>
+                  <li><a href="oreillons.php">oreillons</a></li>
+                  <li><a href="rubéole.php">rubéole</a></li>
+                  <li><a href="méningocoque.php">méningocoque c</a></li>
+                  <li><a href="pneumocoque.php">pneumocoque</a></li>
+
+              </ul>
+            </div>
+          </nav>
+          <h2>Recommandés:</h2>
+    </header>
+  </body>
+</html>
