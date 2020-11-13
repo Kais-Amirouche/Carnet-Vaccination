632f134a5e702e00a1a77bf7eac829859ecc7f62
Amirouche Kais <amirouche@amirouche.home>
2020-11-13 09:07:31 +0100

    work

--- inc/footer.php
+++ inc/footer.php
@@ -5,7 +5,8 @@
   <footer>
     <div class="contact">
       <?php if(isLogged()) { ?>
-      <p>Besoin d'aide ? Posez-vos <a href="contact.php">questions</a></p>
+      <p><a href="contact.php">Besoin d'aide?</a></p>
+      <br>
       <p><a href="mesquestions.php">Mes questions</a></p>
       <?php } ?>
     </div>
