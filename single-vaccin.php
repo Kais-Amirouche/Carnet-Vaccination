ede11cb8c6286804d195139497ce64678a6e93d1
safichou3 <>
2020-11-13 11:52:08 +0100

    update

--- single-vaccin.php
+++ single-vaccin.php
@@ -8,26 +8,42 @@
   </head>
   <body>
     <header>
-      <h1>Les vaccins</h1>
-      <h2>Obligatoires:</h2>
-          <nav>
-              <ul>
-                  <li><a href="Diphtérie.php">Diphtérie</a></li>
-                  <li><a href="tetanos.php">Tétanos</a></li>
-                  <li><a href="Poliomyélite.php">Poliomyélite</a></li>
-                  <li><a href="influenzae.php">l’Haemophilus influenzae B </a></li>
-                  <li><a href="Coqueluche.php">Coqueluche</a></li>
-                  <li><a href="Hépatite.php">Hépatite B </a></li>
-                  <li><a href="rougeole.php">rougeole</a></li>
-                  <li><a href="oreillons.php">oreillons</a></li>
-                  <li><a href="rubéole.php">rubéole</a></li>
-                  <li><a href="méningocoque.php">méningocoque c</a></li>
-                  <li><a href="pneumocoque.php">pneumocoque</a></li>
+      <div class="section">
+        <h2>La Diphtérie</h2>
+          <p><span>- Quand ? :</span>
+            la vaccination DTP est simplifiée et ne comporte plus que deux doses, injectées respectivement à l'âge de 2 mois et de 4 mois, suivies d'un rappel à 11 mois. Des rappels ultérieurs sont recommandés à 6 ans, entre 11 et 13 ans, à 25 ans, 45 ans, 65 ans, puis tous les dix ans chez les adultes de plus de 65 ans. <br>
+            <span>- Causes:</span> se transmet par les gouttelettes de salive émises lorsqu’un malade parle, tousse ou crache, mais également par l’intermédiaire d’objets ou d’aliments contaminés. <br>
+            <span>- Symptômes :</span> la maladie se manifeste par une angine : irritation de la gorge, fièvre, gonflement des ganglions du cou. <br>
+            <span>- Conséquences :</span>
+            inflammation du muscle cardiaque ou myocardite (impose la surveillance régulière de l'électrocardiogramme) ; Troubles neurologiques : principalement des paralysies diverses se manifestant par des troubles de la déglutition et de la dépression respiratoire ; Inflammation du foie ; Altération de la fonction rénale.
+          </p>
+        </div>
 
-              </ul>
-            </div>
-          </nav>
-          <h2>Recommandés:</h2>
+      <div class="sectionx">
+        <h2>Le Tétanos</h2>
+          <p><span>- Quand  ?:</span>
+             aux bébés dès l’âge de 2 mois;
+             aux enfants (à 12 mois, puis entre 4 et 7 ans);
+             aux adolescents (11-13 ans);
+             aux jeunes adultes de 25 à 29 ans;
+             aux adultes et aux seniors (tous les 20 ans pour les vaccins reçus entre 25 et 64 ans, puis tous les 10 ans).
+             <br>
+             <span>- Causes :</span>
+             Le tétanos se contracte par l'infection d'une plaie ou d'une blessure par des spores de la bactérie Clostridium tetani. <br>
+             <span>- Symptômes : </span>
+             crampes au niveau des mâchoires ou incapacité à ouvrir la bouche ;
+             spasmes musculaires souvent dans le dos, l’abdomen et les extrémités ;
+             spasmes musculaires soudains et douloureux, souvent déclenchés par des bruits soudains ;
+             difficultés à avaler ;
+             convulsions ;
+             maux de tête ;
+             fièvre et transpiration ;
+             modification de la tension artérielle ou accélération du rythme cardiaque.
+             <br>
+             <span>- Conséquences : </span>
+             cette maladie cause des spasmes et une paralysie progressive du système musculaire. Le tétanos est fatal dans à peu près 30 % des cas.
+          </p>
+        </div>              
     </header>
   </body>
 </html>
