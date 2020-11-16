f6f63b0c6994c39554d797383724ec63d827ad92
Amirouche Kais <amirouche@amirouche.home>
2020-11-16 10:55:22 +0100

    deroulant

--- inc/footer.php
+++ inc/footer.php
@@ -2,22 +2,21 @@
 
 
   </div>  <!-- container -->
-    <footer>
-      <div class="foot-bg">
-        <div class="foot">
-          <nav>
-            <ul>
-              <div class="contact">
-                <li><a href="contact.php">Besoin d'aide?</a></li>
-                <?php if(isLogged()) { ?>
-                <br>
-                <li><a href="mesquestions.php">Mes questions?</a></li>
-                <?php } ?>
-              </div>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </footer>
+  <footer class="footer-world">
+  			<div class="footer-right">
+  				<a href="#"><img src="asset\img\reseau-facebook.png" alt=""></a>
+  				<a href="#"><img src="asset\img\reseau-instagram.png" alt=""></a>
+  				<a href="#"><img src="asset\img\reseau-twitter.png" alt=""></a>
+  				<a href="#"><img src="asset\img\reseau-whatsapp.png" alt=""></a>
+  			</div>
+  			<div class="footer-left">
+  				<p class="footer-links">
+  					<a class="lien1" href="#">Home</a>
+  					<a href="#">About</a>
+  					<a href="contact.php">Contact</a>
+  				</p>
+  				<p>mon carnet vaccins &copy; 2020</p>
+  			</div>
+  </footer>
   </body>
 </html>
