523494915c9510d382cc7de1aa8c7d8db317edcd
rakotoarisoa <steven.harrissonrah@gmail.com>
2020-11-16 15:31:08 +0100

    oue

--- index.php
+++ index.php
@@ -4,11 +4,6 @@ session_start();
 
 include('inc/pdo.php');
 include('inc/function.php');
-<<<<<<< HEAD
-
-=======
-// debug($_SESSION);
->>>>>>> 5baea6b262d1f7dbedcbae1c27865523b8ac7186
 
 include('inc/header.php');?>
 
