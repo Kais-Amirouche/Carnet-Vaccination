5becf0c8389bbd6c7e28206e13363c1cef0842b2
m02xime <maxkb02@gmail.com>
2020-11-16 09:24:08 +0100

    css

--- inc/footer.php
+++ inc/footer.php
@@ -21,32 +21,3 @@
   </footer>
   </body>
 </html>
-=======
-    <footer>
-      <div class="foot-bg">
-        <div class="foot">
-          <nav>
-            <ul>
-              <div class="contact">
-                <li><a href="contact.php">Besoin d'aide?</a></li>
-                <?php if(isLogged()) { ?>
-                <br>
-                <li><a href="mesquestions.php">Mes questions?</a></li>
-                <?php } ?>
-              </div>
-                <div class="column">
-                  <ul class="social-icons">
-                    <li><a href="#" class="facebook"></a></li>
-                    <li><a href="#" class="twitter"></a></li>
-                    <li><a href="#" class="dribble"></a></li>
-                    <li><a href="#" class="rss"></a></li>
-                  </ul>
-                </div>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </footer>
-    </body>
-  </html>
->>>>>>> b2a1d060443ddaa2a52b29138e9ae277d208f46b
