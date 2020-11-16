8f92b7c9a1dccf9132e3c815567d569886557ec9
rakotoarisoa <steven.harrissonrah@gmail.com>
2020-11-16 12:52:37 +0100

    oue

--- inc/header.php
+++ inc/header.php
@@ -48,6 +48,22 @@
        <?php } ?>
       </div>
     </div>
-    <img class="backgroundheader" src="asset/img/visuel-defenses.jpg" alt="">
+    <div class="presentation">
+      <img class="backgroundheader" src="asset/img/visuel-defenses.jpg" alt="">
+      <p>Mon carnet de vaccination électronique
+Pour être mieux vacciné, sans défaut ni excès
+Le carnet de vaccination est un carnet dans lequel sont notées toutes les vaccinations d’une personne. Le professionnel de santé qui vous vaccine (médecin ou infirmier) écrit dans ce carnet :
+
+le nom du vaccin ;
+la date de l’injection ;
+le numéro du lot (étiquette) ;
+la date du prochain vaccin à faire.
+Ce carnet est très pratique : il vous permet de savoir quelles vaccinations vous avez reçues et si vous êtes à jour de vos vaccinations. Il faut donc le ranger précieusement pour ne pas le perdre et ne pas oublier de le présenter au professionnel de santé à chaque fois que vous vous faites vacciner. Il est valable toute la vie !
+
+Pour l’obtenir, il suffit de le demander aux professionnels de santé. Il est gratuit.
+
+À noter : Pour les enfants, le carnet de santé sert aussi de carnet de vaccination. Le carnet de vaccination remplace le carnet de santé lorsque l’on est adulte (surtout si on ne retrouve plus son carnet de santé).</p>
+
+    </div>
   </header>
   <div class="container">
