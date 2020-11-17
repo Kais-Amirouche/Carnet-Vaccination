2aa40b8680e21378e96ea33f817f104a2336c313
m02xime <maxkb02@gmail.com>
2020-11-17 15:47:04 +0100

    addvaccin update

--- carnet.php
+++ carnet.php
@@ -10,20 +10,20 @@ $title = 'Mon Carnet';
 include('inc/header.php'); ?>
   <div class="carnet">
 
-    <h2>Mon Carnet de Vaccination</h2>
+    <h1>Mon Carnet de Vaccination</h1>
 
     <table id="customers">
         <tr>
-          <th>Mes Vaccin</th>
+          <th>Mes Vaccins</th>
           <th>status</th>
           <th>Doses</th>
           <th>Rappel</th>
         </tr>
         <tr>
-          <td><?php echo $_SESSION['user']['id']; ?></td>
-          <td><?php echo $_SESSION['user']['id']; ?></td>
-          <td><?php echo $_SESSION['user']['id']; ?></td>
-          <td><?php echo $_SESSION['user']['id']; ?></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
         </tr>
         <tr>
           <th><a href="addvaccin.php">Ajouter un vaccin</a></th>
