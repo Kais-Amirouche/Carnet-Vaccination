5bff1541dd2f5b67972cf217b07c45a590b22da2
m02xime <maxkb02@gmail.com>
2020-11-18 16:54:38 +0100

    css update

--- inc/header.php
+++ inc/header.php
@@ -7,7 +7,7 @@
   <link rel="stylesheet" href="asset/css/style.css">
   <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
-  <title>vacciTEK</title>
+  <title>vaccitek</title>
 </head>
 <body>
   <header>
